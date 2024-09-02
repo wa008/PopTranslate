03013aed07a2aff88c469faa5666e828c16fd191
wa008 <2864436211@qq.com>
2024-09-02 22:39:57 +0800

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 var window_id = -1;
+var previous_selection = "";
+
+console.log(window.navigator.language);
+
+async function getLocalParameter(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    return json;
+}
 
 function CreateDiv(){
     console.log("Created begin");
@@ -8,17 +20,18 @@ function CreateDiv(){
     div.style.display = "none";
     div.style.width = "20%";
     div.style.height = "20%";
-    div.style.top = '20px';
-    div.style.right = '20px';
+    div.style.top = '10px';
+    div.style.right = '10px';
     div.style.backgroundColor = "rgba(255,255,255,1)";
     div.style.zIndex = "999999999";
-
-    // 设置内边距和边框
     div.style.padding = "10px"; // 增加内边距
-    div.style.border = "10px solid"; // 设置边框宽度 
-    div.style.overflow = "auto"; // 允许内容滚动 
+    div.style.overflow = "auto"; // 允许内容滚动
+    div.style.resize = 'vertical'; // make div scalable
+    div.style.overflow = 'auto'; // make div scalable
 
-    div.innerHTML = "Apple<br>苹果 手机品牌<br>苹果 一种水果";
+    div.style.boxShadow = "1px 1px 10px 5px gray" // 阴影
+
+    div.innerHTML = "Loading...";
     document.body.insertBefore(div, document.body.firstChild);
     console.log("Created end");
 }
@@ -35,35 +48,64 @@ function removeOverlay(){
     if (document.getElementById("my_overlay").style.display != "none") {
         console.log("Removing overlay!");
         document.getElementById("my_overlay").style.display = "none";
+        document.getElementById("my_overlay").innerHTML = "Loading...";
     }
 };
 
-window.addEventListener('mouseup', function () {
+function isInTargetDiv(evt) {
+    var rect = document.getElementById("my_overlay").getBoundingClientRect();
+    if (evt.clientY >= rect.top && evt.clientY <= rect.bottom && 
+                evt.clientX >= rect.left && evt.clientX <= rect.right) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+async function requestTranslation(selection) {
+    let gitignore_path = chrome.runtime.getURL(".gitignore")
+    console.log('gitignore_path ' + gitignore_path);
+    res_json = await getLocalParameter(gitignore_path)
+    google_translation_api_key = res_json['google_translation_api_key'];
+    let url = "https://translation.googleapis.com/language/translate/v2?key=" + google_translation_api_key;  // Modified
+    console.log(url);
+    console.log('debug 1' + selection);
+    let response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({  // Modified 
+            target: "zh-CN",
+            q: selection
+        }),
+    });
+    console.log('debug requet end');
+    let response_json = await response.json();
+    console.log(response_json);
+    res_text = response_json['data']['translations'][0]['translatedText'];
+    return res_text;
+}
+
+window.addEventListener('mouseup', function (evt) {
+    console.log('mouseup');
     let selection = window.getSelection().toString();
-    if (selection.length > 0) {
-        console.log("select message: " + selection);
+    if (isInTargetDiv(evt) == false && selection.length > 0 && selection != ' ') {
         openOverlay();
-        // chrome.runtime.sendMessage(
-        //     {'key': 'createWindow'}, 
-        //     (response) => {
-        //         console.log('received create window ', response);
-        //         window_id = response.windows_id
-        //     }
-        // );
+        console.log("select message: " + selection);
+        previous_selection = selection;
+        (async() => {
+            let res_text = await requestTranslation(selection)
+            console.log(res_text);
+            document.getElementById("my_overlay").innerHTML = res_text;
+        })()
     }
 });
 
-window.addEventListener('mousedown', function () {
-    console.log('mousedown'); 
-    removeOverlay();
-    // console.log(window_id); 
-    // if (window_id != -1) {
-    //     chrome.runtime.sendMessage(
-    //         {'key': 'removeWindow', 'value': window_id}, 
-    //         (response) => {
-    //             console.log('received remove', response);
-    //         }
-    //     );
-    //     window_id = -1;
-    // }
+window.addEventListener('mousedown', function (evt) {
+    if (isInTargetDiv(evt)) {
+        console.log('in rect');
+    } else {
+        console.log('out rect');
+        console.log('mousedown'); 
+        removeOverlay();
+    }
 });
+
